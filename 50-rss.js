8eb479d320279844f86a756d519cff92fb027408
Danny Su <contact@dannysu.com>
2016-07-25 18:38:27 -0700

    First working version

--- 50-rss.js
+++ 50-rss.js
@@ -7,12 +7,51 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
 
         const node = this;
+        this.feedOptionRules = config.feedOptionRules;
+        this.itemOptionRules = config.itemOptionRules;
         node.on('input', function(msg) {
             try {
-                const feed = new RSS(msg.payload.feedOptions);
+                const items = msg.payload;
 
-                for (let item of msg.payload.items) {
-                    feed.item(item.itemOptions);
+                // Construct feed options based on rules configured
+                const feedOptions = {};
+                for (let rule of node.feedOptionRules) {
+                    if (rule.type === 'str') {
+                        feedOptions[rule.name] = rule.value;
+                    }
+                    else if (rule.type === 'msg') {
+                        feedOptions[rule.name] = RED.util.getMessageProperty(msg, rule.value);
+                    }
+                    else if (rule.type === 'flow') {
+                        feedOptions[rule.name] = node.context().flow.get(rule.value);
+                    }
+                    else if (rule.type === 'global') {
+                        feedOptions[rule.name] = node.context().global.get(rule.value);
+                    }
+                }
+
+                const feed = new RSS(feedOptions);
+
+                for (let item of items) {
+                    const itemOptions = {};
+
+                    // Construct item options based on rules configured
+                    for (let rule of node.itemOptionRules) {
+                        if (rule.type === 'str') {
+                            itemOptions[rule.name] = rule.value;
+                        }
+                        else if (rule.type === 'msg') {
+                            itemOptions[rule.name] = item[rule.value] || '';
+                        }
+                        else if (rule.type === 'flow') {
+                            itemOptions[rule.name] = node.context().flow.get(rule.value);
+                        }
+                        else if (rule.type === 'global') {
+                            itemOptions[rule.name] = node.context().global.get(rule.value);
+                        }
+                    }
+
+                    feed.item(itemOptions);
                 }
 
                 // Overwrite the payload but otherwise keep the message intact
